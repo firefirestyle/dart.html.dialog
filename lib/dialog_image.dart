a23ca72708f883f4cba6382567cfea6a3a5cd85a
kyorohiro <kyorohiro@gmail.com>
2016-10-21 09:40:38 +0900

    ..

--- lib/dialog_image.dart
+++ lib/dialog_image.dart
@@ -3,11 +3,11 @@ part of dialog;
 class ImgageDialog {
   Dialog base;
   String dialogName;
-  String fileBtnId;
-  String uploadBtnId;
+//  String fileBtnId;
+//  String uploadBtnId;
   String closeBtn;
 
-  ImgageDialog({this.dialogName: "dialog_load_img", this.fileBtnId: "fileBtn", this.uploadBtnId: "uploadBtn", this.closeBtn: "closeBtn"}) {
+  ImgageDialog({this.dialogName: "dialog_load_img", this.closeBtn: "closeBtn"}) {
     base = new Dialog(this.dialogName);
   }
 
@@ -15,42 +15,41 @@ class ImgageDialog {
     base.init();
   }
 
-  show({String src:"", bool isCancel:true, Future<bool> onUpdated(ImgageDialog dialog, String src): null,int zIndex:19999}) {
+  Future<String> show({String src:"", bool isCancel:true,int zIndex:19999}) {
+    Completer completer = new Completer();
     html.ImageElement imageTmp = null;
     List<String> c = [
       """<h3>Image Uploader</h3>""", //
-      """<input id="${this.fileBtnId}" style="display:block" type="file">""",
+      """<input id="${dialogName}_file" style="display:block" type="file">""",
       """<div id="imgCont"></div>""",
-      """<button id="${this.uploadBtnId}" style="display:none; padding: 12px 24px;">upload</button>""",
-      """<button id="${this.closeBtn}" style="display:inline; padding: 12px 24px;">close</button>""",
+      """<button id="${dialogName}_upload" style="display:none; padding: 12px 24px;">upload</button>""",
+      """<button id="${dialogName}_close" style="display:inline; padding: 12px 24px;">close</button>""",
     ];
     html.Element elm = base.show(c.join("\r\n"),zIndex);
-    var uploadBtn = elm.querySelector("#${this.uploadBtnId}");
+    var uploadBtn = elm.querySelector("#${dialogName}_upload");
 
     //
     bool click = false;
     uploadBtn.onClick.listen((_) async {
       if(click == true) {
-        return;
+        return "";
       }
       click = true;
       uploadBtn.style.display = "none";
       try {
-        if (onUpdated != null) {
-          if(true == await onUpdated(this, imageTmp.src)) {
-            this.close();
-          }
-        }
+          this.close();
       } finally {
         click = false;
         uploadBtn.style.display = "inline";
+        completer.complete(imageTmp.src);
       }
     });
-    var closeBtn = elm.querySelector("#${this.closeBtn}");
+    var closeBtn = elm.querySelector("#${dialogName}_close");
     closeBtn.onClick.listen((_) {
       this.close();
+      completer.complete("");
     });
-    var fileBtn = elm.querySelector("#${this.fileBtnId}");
+    var fileBtn = elm.querySelector("#${dialogName}_file");
     fileBtn.onChange.listen((html.Event e) async {
       if (fileBtn.files.length == 0) {
         return;
@@ -60,8 +59,9 @@ class ImgageDialog {
       imageTmp = await ImageUtil.resizeImage(await ImageUtil.loadImage(fileBtn.files[0]));
       imageTmp.id = "currentImage";
       base.content.querySelector("#imgCont").children.add(imageTmp);
-//      elm.children.add(imageTmp);
     });
+
+    return completer.future;
   }
 
   close() {
